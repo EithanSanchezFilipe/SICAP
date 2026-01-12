5480d113a4c5e1f4650f4a28b4b3ebfb89319345
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2026-01-12 13:23:05 +0100

    typo: format code

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -3,7 +3,6 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-
 const DATABASE_URL = process.env.DATABASE_URL;
 if (!DATABASE_URL) throw new Error("DATABASE_URL is not defined in .env");
 
