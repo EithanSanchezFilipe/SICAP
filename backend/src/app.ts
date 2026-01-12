2636ce3f1c6434bd5f5aad697c9795a59e68ba1c
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2026-01-12 12:41:04 +0100

    feat: create a small server with express using typescript

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express"
+
+const port = process.env.PORT || "3000"
+const host = process.env.HOST || "localhost"
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(port, () =>{
+    console.log(`Server is running on http://${host}:${port}`)
+})
