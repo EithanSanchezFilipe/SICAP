d89915f33955b4de703f10a632619d6210caacdf
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2026-01-12 13:23:05 +0100

    typo: format code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,12 @@
-import express from "express"
+import express from "express";
 
-const port = process.env.PORT || "3000"
-const host = process.env.HOST || "localhost"
+const port = process.env.PORT || "3000";
+const host = process.env.HOST || "localhost";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.listen(port, () =>{
-    console.log(`Server is running on http://${host}:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
