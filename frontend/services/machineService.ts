21b2b465040134af434b1112b3b8b6e14afb26f4
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2026-01-15 16:34:28 +0100

    feat: create the service to fetch the api

---
+++ frontend/services/machineService.ts
@@ -0,0 +1,13 @@
+import api from "@/lib/api";
+
+export const machineService = {
+  async getAll() {
+    const response = await api.get("/machine");
+    return response.data;
+  },
+
+  async getById(id: number) {
+    const response = await api.get(`/machine/${id}`);
+    return response.data;
+  },
+};
